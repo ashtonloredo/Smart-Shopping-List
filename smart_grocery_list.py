07ef2b7daabcf47f7f9aea74520f3a0ab7fea750
ashtonloredo <ashtonloredo@gmail.com>
2026-04-19 03:35:48 -0500

    fixed inconsistent input normalization
    I have implemented a helper function at the top of the script to simplify the cleanup of each user input to ensure that all input are stripped and in lowercase without having to write both string methods each time.

--- smart_grocery_list.py
+++ smart_grocery_list.py
@@ -3,28 +3,31 @@ shopping_list = {}
 yes_values = {'yes', 'y', 'yeah', 'yep'}
 no_values = {'no', 'n', 'nah', 'nope'}
 
+def clean(text):
+    return text.strip().lower()
+
 while True:
 
     if not shopping_list:
-        response = input("Would you like to add an item? ").strip().lower()
+        response = clean(input("Would you like to add an item? "))
         if response in no_values:
             break
 
-        item = input("What would you like to add? ")
+        item = clean(input("What would you like to add? "))
         shopping_list[item] = 1
 
     else:
-        action = input("What would you like to do? Add? Or remove? ").strip().lower()
+        action = clean(input("What would you like to do? Add? Or remove? "))
 
         if action == 'add':
-            item = input("What would you like to add? ")
+            item = clean(input("What would you like to add? "))
             if item in shopping_list:
                 shopping_list[item] += 1
             else:
                 shopping_list[item] = 1
 
         elif action == 'remove':
-            item = input("What would you like to remove? ")
+            item = clean(input("What would you like to remove? "))
             if item in shopping_list:
                 shopping_list[item] -= 1
                 if shopping_list[item] == 0:
@@ -33,7 +36,7 @@ while True:
                 print("item not found.")
 
         elif action == 'finalize':
-            confirm = input("Finalize list? ").strip().lower()
+            confirm = clean(input("Finalize list? "))
             if confirm in yes_values:
                 break
 
