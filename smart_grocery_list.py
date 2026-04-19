94bed7df8854ccf9f80f16dc0ffec14bf0e88391
ashtonloredo <ashtonloredo@gmail.com>
2026-04-19 03:25:08 -0500

    Duplicate problem fix
    I have now changed the shopping list from a list to a dictionary to ensure that the program can count duplicates as addition in quantity rather than a simple bug.
    
    The rest of the logic in the code was also fixed. Instead of logic that would work on a list item, I made it so that it could handle dictionary data.
    
    Also removed some inconsistencies like the dict variable: "remove_values" and the empty string: "items_string".

--- smart_grocery_list.py
+++ smart_grocery_list.py
@@ -1,32 +1,34 @@
-user_list = []
+shopping_list = {}
 
 yes_values = {'yes', 'y', 'yeah', 'yep'}
 no_values = {'no', 'n', 'nah', 'nope'}
 
-remove_values = {}
-items_string = ""
-
 while True:
 
-    if not user_list:
+    if not shopping_list:
         response = input("Would you like to add an item? ").strip().lower()
         if response in no_values:
             break
 
         item = input("What would you like to add? ")
-        user_list.append(item)
+        shopping_list[item] = 1
 
     else:
         action = input("What would you like to do? Add? Or remove? ").strip().lower()
 
         if action == 'add':
             item = input("What would you like to add? ")
-            user_list.append(item)
+            if item in shopping_list:
+                shopping_list[item] += 1
+            else:
+                shopping_list[item] = 1
 
         elif action == 'remove':
             item = input("What would you like to remove? ")
-            if item in user_list:
-                user_list.remove(item)
+            if item in shopping_list:
+                shopping_list[item] -= 1
+                if shopping_list[item] == 0:
+                    shopping_list.pop(item)
             else:
                 print("item not found.")
 
@@ -38,5 +40,6 @@ while True:
         else:
             print("invalid option.")
     
-    print("Current items:", " ".join(user_list), ",")
+    for item, count in shopping_list.items():
+        print(f'{item} x{count}.')
                     
