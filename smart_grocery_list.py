ec9ebc53686b11c7eea37e3bafe72f12e3dba587
ashtonloredo <ashtonloredo@gmail.com>
2026-04-29 23:10:29 -0500

    Update smart_grocery_list.py
    So far, I have done a few things significant to the project.
    
    1. I removed the variable "yes_values". It noticed it wasn't being used anywhere in the code.
    2. I created a new function called "clean_input" which separates numerical values from alphabetical characters to achieve a clean input by user while allowing the program to manage quantities more easily. This function also gets rid of x as an extra character, as an user could input "apple x3". Thus, the program would interpret this input as "name: apple, quantity: 3".

--- smart_grocery_list.py
+++ smart_grocery_list.py
@@ -1,31 +1,50 @@
 shopping_list = {}
 
-yes_values = {'yes', 'y', 'yeah', 'yep'}
+def clean(text: str):
+    return text
 
-def clean(text):
-    return text.strip().lower()
+def clean_input(text: str):
+    text = text.lower().strip()#.replace(" ", "")
+
+    number = ""
+    name = ""
+
+    for char in text:
+        if char.isdigit():
+            number += char
+        elif char.isalpha() or char.isspace():
+            name += char
+
+    if number:
+        quantity = int(number)
+    else:
+        quantity = 1
+    item = name.replace('x', '').strip()
+
+    
+    return item, quantity
 
 while True:
 
     action = clean(input("What would you like to do? Add? Remove? Or Finalize? "))
 
+    #add 
     if action == 'add':
-        item = clean(input("What would you like to add? "))
+        item, quantity = clean_input(input("What would you like to add? "))
+
+        #In case input is blank or justa space
         if not item:
             print("Invalid item.")
             continue
 
-        if item in shopping_list:
-            shopping_list[item] += 1
-        else:
-            shopping_list[item] = 1
+        shopping_list[item] = shopping_list.get(item, 0) + quantity
 
     elif action == 'remove':
         item = clean(input("What would you like to remove? "))
         if not item:
             print("Invalid item.")
             continue
-            
+
         if item in shopping_list:
             shopping_list[item] -= 1
             if shopping_list[item] == 0:
@@ -35,14 +54,13 @@ while True:
 
     elif action == 'finalize':
         break
-        
+
     else:
         print("invalid option.")
-    
+
     print("\n Current items:")
     if not shopping_list:
         print("empty")
     else:
         for item, count in shopping_list.items():
-            print(f'\u2022 {item.title()} x{count}')
-                    
+            print(f'\u2022 {item.lower()} x{count}')
