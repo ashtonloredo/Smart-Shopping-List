ee3bc5aa58b8928d44fc6a360b9aa853e5c2041d
ashtonloredo <ashtonloredo@gmail.com>
2026-04-30 01:17:32 -0500

    Update smart_grocery_list.py
    Separated the workflow into functions for more readability and leverage.

--- smart_grocery_list.py
+++ smart_grocery_list.py
@@ -1,9 +1,11 @@
 shopping_list = {}
 
-def clean(text: str):
-    return text
+agreement_values = {"yes", "y", "yeah", "yep", "yup"}
 
-def clean_input(text: str):
+def clean(text: str): #Done
+    return text.lower().strip()
+
+def clean_input(text: str): #Done
     text = text.lower().strip()#.replace(" ", "")
 
     number = ""
@@ -24,6 +26,22 @@ def clean_input(text: str):
     
     return item, quantity
 
+def add_item(item, quantity): #Done
+    shopping_list[item] = shopping_list.get(item, 0) + quantity
+
+def remove_item(item, quantity): #Done
+    shopping_list[item] -= quantity
+    if shopping_list[item] <= 0:
+        shopping_list.pop(item)
+
+def display_list():
+    print("\n Current items:")
+    if not shopping_list:
+        print("empty")
+    else:
+        for item, quantity in shopping_list.items():
+            print(f'\u2022 {item.lower()} x{quantity}')
+
 while True:
 
     action = clean(input("What would you like to do? Add? Remove? Or Finalize? "))
@@ -31,36 +49,34 @@ while True:
     #add 
     if action == 'add':
         item, quantity = clean_input(input("What would you like to add? "))
-
         #In case input is blank or justa space
+
         if not item:
             print("Invalid item.")
             continue
+        
+        add_item(item, quantity)
 
-        shopping_list[item] = shopping_list.get(item, 0) + quantity
 
     elif action == 'remove':
-        item = clean(input("What would you like to remove? "))
+        item, quantity = clean_input(input("What would you like to remove? "))
+        
         if not item:
             print("Invalid item.")
             continue
 
-        if item in shopping_list:
-            shopping_list[item] -= 1
-            if shopping_list[item] == 0:
-                shopping_list.pop(item)
+        elif item in shopping_list: 
+            remove_item(item, quantity)
+
         else:
             print("item not found.")
 
     elif action == 'finalize':
-        break
+        response = input("Would you like to see your finalized list? ")
+        if clean(response) in agreement_values:
+            display_list()
+        else:
+            break
 
     else:
         print("invalid option.")
-
-    print("\n Current items:")
-    if not shopping_list:
-        print("empty")
-    else:
-        for item, count in shopping_list.items():
-            print(f'\u2022 {item.lower()} x{count}')
